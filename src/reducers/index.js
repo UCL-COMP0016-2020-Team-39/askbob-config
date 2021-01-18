b364120a4f666b41abf79fc00bff7a3b38309229
Akhere-G <zcabaih@ucl.ac.uk>
2021-01-18 21:23:48 +0000

    AB-8 completed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import questionsReducer from "./questionsReducer";
 import responsesReducer from "./responsesReducer";
 import formReducer from "./formReducer";
 
+
 const reducer = combineReducers({
   questions: questionsReducer,
   responses: responsesReducer,
