a4b41658c569df67ba25b4eabb2665b83117411f
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2021-03-16 11:35:26 +0000

    removed random string added to ids

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,5 @@
 import { v4 } from "uuid";
 export const nameToId = name => {
-  return `${name}${v4()}`
-    .trim()
-    .toLowerCase()
-    .replaceAll(/(\s+)|(-+)/g, "_");
-};
-
-export const nameToIdNonUniqueId = name => {
   return name
     .trim()
     .toLowerCase()
